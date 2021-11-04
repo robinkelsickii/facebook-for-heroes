3f3679b205025a01dc5e568d2b03f11d2592ffdb
robinkelsickii <robinkelsickii@gmail.com>
2021-11-04 18:51:16 -0400

    commit

--- heroes_api.php
+++ heroes_api.php
@@ -66,7 +66,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
-    echo "name: " . $row["name"] . " about_me: " . $row["about_me"]. " " . $row["biograohy"]. "<br>";
+    echo "name: " . $row["name"] . " about_me: " . $row["about_me"]. " " . $row["biography"]. "<br>";
   }
 } else {
   echo "0 results";
